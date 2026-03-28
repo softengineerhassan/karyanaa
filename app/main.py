9d4910dcbce007f0d8f8ad966476f379d3fe343e
Hassan_dev <softengineerhassan@gmail.com>
2026-03-29 02:08:56 +0500

    f

--- app/main.py
+++ app/main.py
@@ -75,7 +75,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(
     title=settings.APP_NAME,
     version=settings.APP_VERSION,
-    description="KARYANA",
+    description="KARYANAA",
     docs_url="/docs" if settings.ENABLE_DOCS else None,
     redoc_url="/redoc" if settings.ENABLE_DOCS else None,
     openapi_url="/openapi.json" if settings.ENABLE_DOCS else None,
